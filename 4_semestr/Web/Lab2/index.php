d099446d2ec6bbc66821ff6d9bf9680fe5f36659
Софья Куликова <113458768+SAKulikova@users.noreply.github.com>
2024-03-03 20:34:49 +0200

    Add #2.
    Work with numbers(int, float)

--- 4_semestr/Web/Lab2/index.php
+++ 4_semestr/Web/Lab2/index.php
@@ -10,3 +10,15 @@ $order .= "Hi";
   
  // Don't change the line below
  echo "\nYour order is: $very_bad_unclear_name.";
+
+// #2
+$number = 15;
+echo $number;
+$digit = 0x1B7;
+echo "\n$digit";
+$float = 1.56;
+echo $float;
+echo (10 + 2);
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "\nРазница между расходами:",$last_month-$this_month;
