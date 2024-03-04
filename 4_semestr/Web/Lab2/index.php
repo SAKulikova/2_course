8ec204fe0a479cf82ff3c06605074605a6e12b36
Софья Куликова <113458768+SAKulikova@users.noreply.github.com>
2024-03-04 18:59:22 +0200

    Done #14
    Mathematical functions

--- 4_semestr/Web/Lab2/index.php
+++ 4_semestr/Web/Lab2/index.php
@@ -43,3 +43,56 @@ $answer /= 2;
 $answer -= $my_num;
 echo $answer;
 
+//#14
+$a = 10;
+$b = 3;
+echo $a%$b;
+if ($a%$b==0){
+    echo "\nДелится, результат деления: ",$a/$b;
+}
+else{
+    echo "\nДелится с остатком ", $a%$b;
+}
+$st = pow(2,10);
+$sqrtOne = sqrt(245);
+$arr = [4, 2, 5, 19, 13, 0, 10];
+$sumOfSquares = 0;
+foreach ($arr as $i){
+    $sumOfSquares += pow($i,2);
+}
+$sqrtSumOfSquares = sqrt($sumOfSquares);
+$n = round(sqrt(379));
+$k = round(sqrt(379),1);
+$t = round(sqrt(379),2);
+$varOne = ceil(sqrt(587));
+$varTwo = floor(sqrt(587));
+$arrRouding = ['ceil' => $varOne, 'floor' => $varTwo];
+$maximum = max(4,-2, 5, 19, -130, 0, 10);
+$minimum = min(4,-2, 5, 19, -130, 0, 10);
+echo rand(1,100);
+$i=0;
+$massive = [];
+while($i<10)
+{
+    $massive[$i] = rand(1,100);
+    $i++;
+}
+$module =  abs($a-$b); //a и b уже заданы выше
+$masOfNumbers = [1, 2, -1, -2, 3, -3];
+foreach ($masOfNumbers as $key => $value) {
+    if ($masOfNumbers[$key]<0){
+        $masOfNumbers[$key] = abs($masOfNumbers[$key]);
+    }
+}
+$num = 30;
+$divider = [];
+for ($i=1;$i<$num+1;$i++) {
+    if ($num%$i == 0){
+        $divider[]=$i;
+    }
+}
+$arrTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for ($sum = 0, $i = 0; $sum <= 10; $i++) {
+    $sum += $arrTwo[$i];
+}
+
